935c31359d341d8bd1f06dd2654001564cb0a154
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-08-25 11:30:35 +0200

    Added redirect after form submit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Staticman comments
           </Link>
         </h1>
       )
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,9 +26,10 @@ class BlogIndex extends React.Component {
                 <div>
                     <form method="POST"
                           action="https://api.staticman.net/v2/entry/vojtechruz/staticman-example/master/comments">
+                        <input name="options[redirect]" type="hidden" value="https://staticman-example.netlify.com/"/>
                         <label><input name="fields[name]" type="text"/>Name</label>
                         <label><input name="fields[email]" type="email"/>E-mail</label>
-                        <label><textarea name="fields[message]"></textarea>Message</label>
+                        <label><textarea name="fields[message]"/>Message</label>
 
                         <button type="submit">Submit</button>
                     </form>
